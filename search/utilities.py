a43e189975b71dea8f60c827cde672d8070405ea
Rahul Singh <techizydevs.in@gmail.com>
2019-10-25 04:02:29 +0530

    changing weights for better evaluation and adding comments

--- search/utilities.py
+++ search/utilities.py
@@ -5,7 +5,7 @@ w1 = 0.2
 # rank weight for word length
 w2 = 0.1
 # rank weight for words with higher frequency
-w3 = -1
+w3 = -0.25
 # upper bound on the maximum number of word suggestions to be sent as response
 limit = 25
 # index of word to be used as key during sorting of tuples
@@ -25,29 +25,33 @@ def sort(tuples):
 def getData(sub_string):
     # loading the data into data frame.
     wordFreqTuple = []
-    meanFreq = 0
+    
     with open('word_search.tsv') as datafile:  # opening the tab sepearted value file
         for row in datafile:
             word, frequency = row.split('\t')
             if sub_string in word:  # matching words which contain the substring
                 wordFreqTuple.append((word, math.log(int(frequency), 2)))  # converting word frequency into logarithm for more meaningful evaluation
-                meanFreq = meanFreq + int(frequency)
-    meanFreq = meanFreq/len(wordFreqTuple)
-    return wordFreqTuple, meanFreq
+    minFreq = 0
+    maxFreq = 0
+    if wordFreqTuple != []:
+        freqList = [tupl[1] for tupl in wordFreqTuple]
+        minFreq = min(freqList)
+        maxFreq = max(freqList)
+    medianFreq = (minFreq + maxFreq) / 2
+    return wordFreqTuple, medianFreq
 
 
 def getWordsWith(sub_string):
-    wordFreqTuple, meanFreq = getData(sub_string)
+    wordFreqTuple, medianFreq = getData(sub_string)
     finallist = []
     # for handling errors if the input value is not part of any words
     if len(wordFreqTuple) != 0:
         wordRankList = []
-        meanFreq = meanFreq / len(wordFreqTuple)
         for tupl in wordFreqTuple:
             substrIndex = tupl[0].index(sub_string)
-            rank = (substrIndex * w1) + (len(tupl[0]) * w2) + ((math.log(tupl[1], 2) / meanFreq) * w3)
+            rank = (substrIndex * w1) + (len(tupl[0]) * w2) + ((tupl[1] / medianFreq) * w3)  # aggregate rank for a word
             wordRankList.append((tupl[0], rank))
-        wordRankList = sort(wordRankList)
+        wordRankList = sort(wordRankList)  # sorting words according to rank
         for tupl in wordRankList:
             finallist.append(tupl[0])
             if len(finallist) == limit:
